6d8fc33b50955c8ad6db5b5c3a1a27d9b028450f
Mohammad Shahzeb <mohammadshahzeb0012@gmail.com>
2024-02-17 20:26:55 +0530

    0.4

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const form = document.getElementById("form");
 const timersList = document.getElementById("current-timers-list");
-const timerText = document.getElementById("timer-text")
+const timerText = document.getElementById("timer-text");
 
 let uniqueId = 0;
 let numberOfActiveTimers = 0;
@@ -8,8 +8,11 @@ let numberOfActiveTimers = 0;
 function updateTime(hh, mm, ss) {
     let totalTime = hh * 3600 + mm * 60 + ss;
     if (totalTime > 0) {
-        create_element(hh, mm, ss,totalTime);
-        numberOfActiveTimers++
+        createTimerElement(hh, mm, ss, totalTime);
+        numberOfActiveTimers++;
+        if (numberOfActiveTimers > 0) {
+            timerText.style.display = "none"
+        }
     }
 }
 
@@ -20,63 +23,59 @@ form.addEventListener("submit", (e) => {
     let hh = parseInt(document.getElementById("hour").value);
     let mm = parseInt(document.getElementById("minute").value);
     let ss = parseInt(document.getElementById("second").value);
-    form.reset()
+    form.reset();
     updateTime(hh, mm, ss);
 });
 
-function create_element(hh, mm, ss , totalTime) {
+function createTimerElement(hh, mm, ss, totalTime) {
     const timersItem = document.createElement("div");
     timersItem.id = `timers-item-${uniqueId}`;
+    timersItem.classList.add("running-timer");
     timersItem.innerHTML = `
-        <div class="running-timer">
-            <p>Time Left:</p>
-            <div class="input-Wrraper">
-                <input type="number" id="h" min="0" max="23" placeholder="hh" value="${hh}" readonly>
-                <p>:</p>
-                <input type="number" id="m" min="0" max="59" placeholder="mm" value="${mm}" readonly>
-                <p>:</p>
-                <input type="number" id="s" min="0" max="59" placeholder="ss" value="${ss}" readonly>
-            </div>
-            <button class="Delete">Delete</button>
+        <p>Time Left:</p>
+        <div class="input-Wrraper">
+            <input type="number" id="h" min="0" max="23" placeholder="hh" value="${hh}" readonly>
+            <p>:</p>
+            <input type="number" id="m" min="0" max="59" placeholder="mm" value="${mm}" readonly>
+            <p>:</p>
+            <input type="number" id="s" min="0" max="59" placeholder="ss" value="${ss}" readonly>
         </div>
+        <button class="Delete">Delete</button>
     `;
     timersList.appendChild(timersItem);
 
-//    console.log(h.parentNode.parentNode)
-
     const deleteButton = timersItem.querySelector(".Delete");
-    deleteButton.addEventListener("click", (e) => {
-        e.target.closest(".running-timer").remove();
+    deleteButton.addEventListener("click", () => {
+        timersItem.remove();
         numberOfActiveTimers--;
+
+        if (numberOfActiveTimers == 0) {
+            timerText.style.display = "flex"
+        }
     });
 
-    
+    runTimer(totalTime, timersItem);
+}
 
+function runTimer(totalTime, timersItem) {
+    let hours = timersItem.querySelector("#h");
+    let minutes = timersItem.querySelector("#m");
+    let seconds = timersItem.querySelector("#s");
 
-   function  runTimer(totalTime){
-    let hours = document.querySelector("#h");
-    let minutes = document.querySelector("#m")
-    let seconds = document.querySelector("#s");
+    let interval = setInterval(() => {
+        if (totalTime > 0) {
+            --totalTime;
 
-     let intervel = setInterval(()=>{
-        if(totalTime >0){
-         --totalTime;
-
-         hours.value = Math.floor(totalTime / 3600);
-
-         minutes.value = Math.floor((totalTime % 3600) / 60);
-         seconds.value = Math.floor(totalTime % 60);
+            hours.value = Math.floor(totalTime / 3600);
+            minutes.value = Math.floor((totalTime % 3600) / 60);
+            seconds.value = Math.floor(totalTime % 60);
+        } else {
+            clearInterval(interval);
+            timersItem.className = "completed-timer";
+            timersItem.innerHTML = `
+                <p>Timer Is Up!</p>
+                <button class="Delete stop">Stop</button>
+            `;
         }
-        else{
-            timersItem.className= "completed-timer"
-            timersItem.innerHTML = ` 
-            <p>Timer Is Up !</p>
-            <button class="Delete stop">Stop</button>
-            `
-        }
-     },1000)
-   }
-   runTimer(totalTime)
-
-
+    }, 1000);
 }
